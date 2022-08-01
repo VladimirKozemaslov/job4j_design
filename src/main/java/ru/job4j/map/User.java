0267980ef07917ff73624b62784d1715bc28414a
VladimirKozemaslov <kvs@intelins.ru>
2022-08-01 13:10:53 +0500

    4. Переопределить только equals [#1004 #326870]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -11,5 +14,37 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>(16);
+        Calendar birthday = Calendar.getInstance();
+
+        User user1 = new User("Vladimir", 1, birthday);
+        int hashCode1 = user1.hashCode();
+        int hash1 = hashCode1 & (hashCode1 >>> 16);
+        int bucket1 = hash1 & 15;
+
+        User user2 = new User("Vladimir", 1, birthday);
+        int hashCode2 = user1.hashCode();
+        int hash2 = hashCode1 & (hashCode1 >>> 16);
+        int bucket2 = hash1 & 15;
+
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+
+        System.out.printf("user1: хэшкод - %s, хэш - %s, бакет - %s", hashCode1, hash1, bucket1);
+        System.out.println();
+        System.out.printf("user2: хэшкод - %s, хэш - %s, бакет - %s", hashCode2, hash2, bucket2);
+        System.out.println();
+        System.out.println(map.size());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
     }
 }
