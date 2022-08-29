2df1d9bf319118749a0a8709beb8df38a03f6420
VladimirKozemaslov <kvs@intelins.ru>
2022-08-29 10:52:39 +0500

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/MultiplicationTable.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("multiplication_table.txt")) {
+            for (int i = 1; i < 10; i++) {
+                for (int j = 1; j < 10; j++) {
+                    String str = i + " * " + j + " = " + i * j;
+                    out.write(str.getBytes());
+                    out.write(System.lineSeparator().getBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
