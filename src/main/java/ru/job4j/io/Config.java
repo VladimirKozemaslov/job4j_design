5bd58f231f82dfda5f0305e9300d1a23ee38e552
VladimirKozemaslov <kvs@intelins.ru>
2022-08-31 15:03:43 +0500

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,60 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            in.lines()
+                    .filter(str -> str.length() != 0 && !str.startsWith("#"))
+                    .peek(str -> {
+                        if (str.startsWith("=")
+                                || ((str.indexOf('=') == str.lastIndexOf('=') && str.charAt(str.length() - 1) == str.lastIndexOf('='))
+                                || str.indexOf('=') == -1)) {
+                            throw new IllegalArgumentException();
+                        }
+                    })
+                    .forEach(str -> {
+                int delimiterIdx = str.indexOf('=');
+                String key = str.substring(0, delimiterIdx);
+                String value = str.substring(delimiterIdx + 1);
+                values.put(key, value);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Vladimir Kozemaslov");
+    }
+
+    @Test
+    void whenPairWithCommentAndEmptyLines() {
+        String path = "./data/pair_with_comment_and_empty_lines.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Elvis Presley");
+    }
+
+    @Test
+    void whenPairWithWrongPattern() {
+        String path = "./data/pair_with_wrong_pattern.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load).isInstanceOf(IllegalArgumentException.class);
+    }
+}
