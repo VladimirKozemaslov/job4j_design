bd0f3619c99e1f203bd979e463b17c8a233758da
VladimirKozemaslov <kvs@intelins.ru>
2022-09-01 17:45:40 +0500

    2. Анализ доступности сервера. [#859 #338633]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,43 +1,40 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.List;
 
 public class Analizy {
+
+    private boolean isServerOff = false;
 
     public void unavailable(String source, String target) {
         StringBuilder sb = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            boolean isServerOff = false;
-            List<String> recList = in.lines().toList();
-            for (String line : recList) {
-                int delimiterIdx = line.indexOf(' ');
-                String status = line.substring(0, delimiterIdx);
-                String date = line.substring(delimiterIdx + 1);
-                if ((status.equals("400") || status.equals("500"))) {
-                    if (!isServerOff) {
-                        isServerOff = true;
-                        sb.append(date).append(";");
-                    }
-                } else {
-                    if (isServerOff) {
-                        isServerOff = false;
-                        sb.append(date).append(System.lineSeparator());
-                    }
-                }
-            }
+            PrintWriter out = new PrintWriter(
+                    new BufferedOutputStream(
+                            new FileOutputStream(target)));
+            in.lines().forEach(str -> analyzeStr(str, sb));
+            out.print(sb);
+            out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                )
-        )) {
-            out.print(sb);
-        } catch (IOException e) {
-            e.printStackTrace();
+    }
+
+    private void analyzeStr(String line, StringBuilder sb) {
+        int delimiterIdx = line.indexOf(' ');
+        String status = line.substring(0, delimiterIdx);
+        String date = line.substring(delimiterIdx + 1);
+        if ((status.equals("400") || status.equals("500"))) {
+            if (!isServerOff) {
+                isServerOff = true;
+                sb.append(date).append(";");
+            }
+        } else {
+            if (isServerOff) {
+                isServerOff = false;
+                sb.append(date).append(System.lineSeparator());
+            }
         }
     }
 
