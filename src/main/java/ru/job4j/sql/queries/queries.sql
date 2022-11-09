a3cf8a5d8506e207ff9351834aaca8dd3cd6fbca
VladimirKozemaslov <kvs@intelins.ru>
2022-11-09 19:49:31 +0500

    0. Select [#6861]

---
+++ src/main/java/ru/job4j/sql/queries/queries.sql
@@ -0,0 +1,26 @@
+create table fauna (
+    id serial primary key,
+    name text,
+    avg_age int,
+    discovery_date date
+);
+
+insert into fauna(name, avg_age, discovery_date) values
+('Catfish', 30000, date '1751-03-20'),
+('Sea turtle', 200000, date '1823-11-05'),
+('Whale', 15000, date '1868-04-15'),
+('Rhino', 18000, date '1966-08-11'),
+('Elephant', 12000, date '1950-02-21'),
+('Wolf', 6000, date '1981-01-09'),
+('Rabbit', 1000, date '1830-03-04'),
+('Parrot', 11000, null),
+('Fox', 1500, null),
+('Bear', 5000, date '1498-02-19');
+
+select * from fauna where name like '%fish';
+
+select * from fauna where avg_age between 10000 and 21000;
+
+select * from fauna where discovery_date is null;
+
+select * from fauna where discovery_date < '1950-01-01';
