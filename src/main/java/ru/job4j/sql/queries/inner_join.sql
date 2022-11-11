41720bd58aa105a225a1b57f0dde6fbf853e7660
VladimirKozemaslov <kvs@intelins.ru>
2022-11-11 15:41:48 +0500

    1. Inner join [#373617 #359631]

--- src/main/java/ru/job4j/sql/queries/inner_join.sql
+++ src/main/java/ru/job4j/sql/queries/inner_join.sql
@@ -16,7 +16,9 @@ create table orders_items(
     c_item int references items(id)
 );
 
-insert into items(name, description, price) values
+insert into
+items(name, description, price)
+values
 ('Black bread', 'Borodinsky bread with cumin', 58),
 ('Lemonade', 'Buratino, 1,5 L', 43),
 ('Meat dumplings', 'Miratorg 800g', 205),
@@ -25,36 +27,64 @@ insert into items(name, description, price) values
 ('Cabbage', 'White', 51),
 ('Pickles', '0.8 L', 74);
 
-insert into orders(delivery_address) values
+insert into
+orders(delivery_address)
+values
 ('Moscow, Lenin st.3 room 41'),
 ('Saint Petersburg, Sennaya st.9 room 125'),
 ('Chelyabinsk, Traktornaya st.9 room 3');
 
-insert into orders_items(c_order, c_item) values
-((select id from orders where delivery_address like 'Moscow%'), (select id from items where name like 'Twix%')),
-((select id from orders where delivery_address like 'Moscow%'), (select id from items where name like 'Meat%')),
-((select id from orders where delivery_address like 'Moscow%'), (select id from items where name like 'Pick%')),
-((select id from orders where delivery_address like 'Saint%'), (select id from items where name like 'Black%')),
-((select id from orders where delivery_address like 'Saint%'), (select id from items where name like 'Lemo%')),
-((select id from orders where delivery_address like 'Saint%'), (select id from items where name like 'Carr%')),
-((select id from orders where delivery_address like 'Saint%'), (select id from items where name like 'Meat%')),
-((select id from orders where delivery_address like 'Chelya%'), (select id from items where name like 'Cabb%')),
-((select id from orders where delivery_address like 'Chelya%'), (select id from items where name like 'Pick%')),
-((select id from orders where delivery_address like 'Chelya%'), (select id from items where name like 'Meat%')),
-((select id from orders where delivery_address like 'Chelya%'), (select id from items where name like 'Black%'));
+select * from orders;
+
+select * from items;
+
+insert into
+orders_items(c_order, c_item)
+values
+(1, 3),
+(1, 3),
+(1, 7),
+(2, 1),
+(2, 2),
+(2, 4),
+(2, 3),
+(3, 6),
+(3, 7),
+(3, 3),
+(3, 1);
 
 select * from orders_items;
 
-select o.delivery_address as Address, i.name as Item, i.description as Description, i.price as Price from
-    items i inner join orders_items oi on i.id = oi.c_item
-    inner join orders o on oi.c_order = o.id;
+select
+o.delivery_address as Address,
+i.name as Item, i.description as Description,
+i.price as Price
+from
+items i inner join orders_items oi
+    on i.id = oi.c_item
+inner join orders o
+    on oi.c_order = o.id;
 
-select o.delivery_address as Address, i.name as Item, i.description as Description, i.price as Price from
-    items i inner join orders_items oi on i.id = oi.c_item
-    inner join orders o on oi.c_order = o.id
+select
+o.delivery_address as Address,
+i.name as Item,
+i.description as Description,
+i.price as Price
+from
+items i inner join orders_items oi
+    on i.id = oi.c_item
+inner join orders o
+    on oi.c_order = o.id
 where i.price < 90;
 
-select o.delivery_address as Address, i.name as Item, i.description as Description, i.price as Price from
-    items i inner join orders_items oi on i.id = oi.c_item
-    inner join orders o on oi.c_order = o.id
+select
+o.delivery_address as Address,
+i.name as Item,
+i.description as Description,
+i.price as Price
+from
+items i inner join orders_items oi
+    on i.id = oi.c_item
+inner join orders o
+    on oi.c_order = o.id
 where i.price < 90 and o.delivery_address like 'M%';
