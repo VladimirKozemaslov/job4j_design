efe7f6db34cef9e191aa8fa5a9f882faba1606f9
VladimirKozemaslov <kvs@intelins.ru>
2022-11-14 23:11:01 +0500

    1. Представления [#504792]

---
+++ src/main/java/ru/job4j/sql/db_objects/views.sql
@@ -0,0 +1,61 @@
+create table students (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into students (name) values ('Иван Иванов');
+insert into students (name) values ('Петр Петров');
+
+create table authors (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into authors (name) values ('Александр Пушкин');
+insert into authors (name) values ('Николай Гоголь');
+
+create table books (
+    id serial primary key,
+    name varchar(200),
+    author_id integer references authors(id)
+);
+
+insert into books (name, author_id) values ('Евгений Онегин', 1);
+insert into books (name, author_id) values ('Капитанская дочка', 1);
+insert into books (name, author_id) values ('Дубровский', 1);
+insert into books (name, author_id) values ('Мертвые души', 2);
+insert into books (name, author_id) values ('Вий', 2);
+
+create table orders (
+    id serial primary key,
+    active boolean default true,
+    book_id integer references books(id),
+    student_id integer references students(id)
+);
+
+insert into orders (book_id, student_id) values (1, 1);
+insert into orders (book_id, student_id) values (3, 1);
+insert into orders (book_id, student_id) values (5, 2);
+insert into orders (book_id, student_id) values (4, 1);
+insert into orders (book_id, student_id) values (2, 2);
+
+select * from students;
+
+
+create view students_with_books as
+select
+s.name,
+count(a.name),
+substring(a.name from 1 for 1) || '. ' || substring(a.name from position(' ' in a.name)) as author
+from students as s
+inner join orders o
+    on s.id = o.student_id
+inner join books b
+    on o.book_id = b.id
+inner join authors a
+    on b.author_id = a.id
+group by (s.name, a.name)
+having count(a.name) >= 1
+order by s.name, count(a.name) desc, author desc;
+
+select * from students_with_books;
