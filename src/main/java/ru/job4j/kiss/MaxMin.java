4310e7d681b901c5a431bb4c2b2361373220cee9
VladimirKozemaslov <kvs@intelins.ru>
2023-02-14 15:54:44 +0500

    2. Принципы Kiss, Dry и Yagni [#238813]

---
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -0,0 +1,37 @@
+package ru.job4j.kiss;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class MaxMin {
+    public <T> T max(List<T> value, Comparator<T> comparator) {
+        return getValue(value, comparator, (num -> num > 0));
+    }
+
+    public <T> T min(List<T> value, Comparator<T> comparator) {
+        return getValue(value, comparator, (num -> num < 0));
+    }
+
+    private <T> T getValue(List<T> value, Comparator<T> comparator, Predicate<Integer> predicate) {
+        T rsl;
+        switch (value.size()) {
+            case 0:
+                rsl = null;
+                break;
+            case 1:
+                rsl = value.get(0);
+                break;
+            default:
+                rsl = value.get(0);
+                for (int i = 1; i < value.size(); i++) {
+                    T item = value.get(i);
+                    if (!predicate.test(comparator.compare(rsl, item))) {
+                        rsl = item;
+                    }
+                }
+                break;
+        }
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.kiss;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.assertj.SimpleCollection;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MaxMinTest {
+    @Test
+    void whenMaxIntegerEquals13() {
+        MaxMin maxMin = new MaxMin();
+        List<Integer> list = List.of(1, 5, 8, 13, -6, 11);
+        assertThat(maxMin.max(list, Integer::compare)).isEqualTo(13);
+    }
+
+    @Test
+    void whenMInIntegerEquals13() {
+        MaxMin maxMin = new MaxMin();
+        List<Integer> list = List.of(1, 5, 8, 13, -6, 11);
+        assertThat(maxMin.min(list, Integer::compare)).isEqualTo(-6);
+    }
+
+    @Test
+    void whenNoElementsThenNull() {
+        MaxMin maxMin = new MaxMin();
+        List<Integer> list = new ArrayList<>();
+        assertThat(maxMin.min(list, Integer::compare)).isNull();
+    }
+
+    @Test
+    void whenOneElementThenElement() {
+        MaxMin maxMin = new MaxMin();
+        List<Integer> list = List.of(11);
+        assertThat(maxMin.min(list, Integer::compare)).isEqualTo(11);
+    }
+}
