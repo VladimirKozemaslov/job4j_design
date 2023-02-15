6a6af484cea664669db731b76b9cd54f4c4f89d9
VladimirKozemaslov <kvs@intelins.ru>
2023-02-15 13:53:16 +0500

    4. Шаблонизатор. [#855 #391861]

--- src/test/java/ru/job4j/ood/template/StringGeneratorTest.java
+++ src/test/java/ru/job4j/ood/template/StringGeneratorTest.java
@@ -10,16 +10,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class StringGeneratorTest extends TestCase {
     @Test
     public void whenResultMatchesHelloBorisMyNameIsVladimir() {
-        StringGenerator generator = new StringGenerator();
+        Generator generator = new StringGenerator();
         String goal = "Hello Boris, my name is Vladimir.";
         String template = "Hello ${name}, my name is ${subject}";
         Map<String, String> args = Map.of("name", "Boris", "subject", "Vladimir");
-        assertThat(goal).isEqualTo(generator.produce(template, args));
+        assertThat(generator.produce(template, args)).isEqualTo(goal);
     }
 
     @Test
     public void whenKeyFromTemplateDoesNotExistInArgs() {
-        StringGenerator generator = new StringGenerator();
+        Generator generator = new StringGenerator();
         String template = "Hello ${name}, my name is ${subject}";
         Map<String, String> args = Map.of("name", "Boris");
         assertThatThrownBy(() -> generator.produce(template, args)).
@@ -28,7 +28,7 @@ public class StringGeneratorTest extends TestCase {
 
     @Test
     public void whenKeyFromArgsDoesNotExistInTemplate() {
-        StringGenerator generator = new StringGenerator();
+        Generator generator = new StringGenerator();
         String template = "Hello ${name}, my name is ${subject}";
         Map<String, String> args = Map.of("name", "Boris", "surname", "Johnson", "subject", "Vladimir");
         assertThatThrownBy(() -> generator.produce(template, args)).
