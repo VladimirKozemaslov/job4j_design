1c4545361dbc6ba32e56330b30ca3bb77ca2d19a
VladimirKozemaslov <kvs@intelins.ru>
2023-02-20 13:16:22 +0500

    4. Шаблонизатор. [#855 #391861]

--- src/test/java/ru/job4j/ood/template/StringGeneratorTest.java
+++ src/test/java/ru/job4j/ood/template/StringGeneratorTest.java
@@ -1,11 +1,13 @@
 package ru.job4j.ood.template;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Disabled
 public class StringGeneratorTest {
     @Test
     public void whenResultMatchesHelloBorisMyNameIsVladimir() {
